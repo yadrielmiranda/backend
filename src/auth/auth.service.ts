4dae72b9e3a0d2970c006aa952438146d4a2501f
Yadriel Miranda Dorta <ymdorta@gmail.com>
2025-06-23 08:30:36 -0400

    cambios para que funcione token cookie

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -12,15 +12,20 @@ export class AuthService {
 
   async login(username: string, pass: string): Promise<{ access_token: string }> {
     const user = await this.usersService.user({ username });
+    console.log("ESTE ES EL USER");
+    console.log(user);
+    
+    
     const isMatch = await bcrypt.compare(pass, user.password);
 
 
     if (!isMatch) {
       throw new UnauthorizedException();
     }
-    const payload = { userID: user.id, userName: user.username };
+    const payload = { userID: user.id, userName: user.username, userFirstName: user.firstName, userLastName: user.lastName, userEmail: user.email };
     return {
-      access_token: await this.jwtService.signAsync(payload)
+      access_token: await this.jwtService.signAsync(payload),
+     
     }
 
 
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,8 +17,15 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
-  
-  app.enableCors();
+
+  app.enableCors(
+    {
+      origin: 'http://localhost:3000', // Tu frontend Next.js
+      credentials: true, // 
+    }
+  );
+
+  app.use(cookieParser()); // Usa cookie-parser para que NestJS pueda trabajar con cookies
 
   await app.listen(process.env.PORT ?? 4000);
 }
