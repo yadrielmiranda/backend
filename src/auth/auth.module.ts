d1dee77285d04533378a631bea63165aabadb072
Yadriel Miranda Dorta <ymdorta@gmail.com>
2025-06-23 22:03:26 -0400

    tiempo de valides del token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constantes';
+import { jwtConstants } from './constantesw';
 
 @Module({
   imports: [
@@ -11,7 +11,7 @@ import { jwtConstants } from './constantes';
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: {expiresIn: '120s'}
+      signOptions: {expiresIn: '1d'}
     })
   ],
   controllers: [AuthController],
--- src/auth/guards/auth/auth.guard.ts
+++ src/auth/guards/auth/auth.guard.ts
@@ -1,7 +1,7 @@
 import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
-import { jwtConstants } from 'src/auth/constantes';
+import { jwtConstants } from 'src/auth/constantesw';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
