6d972de221c983f069216b55036cd4acd973e641
Yadriel Miranda Dorta <ymdorta@gmail.com>
2026-05-08 01:23:48 -0400

    sin mostrar consol-log

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -91,9 +91,7 @@ export class AuthController {
       userAgent: req.headers['user-agent'] as string | undefined,
       ip: req.ip as string | undefined,
     });
-    console.log('SET COOKIE DOMAIN =>', process.env.COOKIE_DOMAIN);
-
-
+    
     res.cookie(ACCESS_COOKIE, accessToken, this.cookieOptions(1000 * 60 * 15)); // 15m
     res.cookie(
       REFRESH_COOKIE,
@@ -146,7 +144,7 @@ export class AuthController {
         source: 'AuthController.logout',
       });
     }
-    console.log('CLEAR COOKIE DOMAIN =>', process.env.COOKIE_DOMAIN);
+    
     res.cookie(ACCESS_COOKIE, '', this.clearCookieOptions());
     res.cookie(REFRESH_COOKIE, '', this.clearCookieOptions());
 
