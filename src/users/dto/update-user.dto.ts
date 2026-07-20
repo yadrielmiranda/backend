40e32d022eb3f2a8c875a85c475e5c99f73b9ef7
Yadriel Miranda Dorta <ymdorta@gmail.com>
2026-07-20 10:59:21 -0400

    markup de user acepta negativo

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,12 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
-import { IsBoolean, IsNumber, IsOptional, Min } from 'class-validator';
+import { IsBoolean, IsNumber, IsOptional } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   @IsOptional()
   @IsNumber()
-  @Min(0)
   @Type(() => Number)
   markupOverride?: number | null;
 
